8b4e146ce588f741c8bbd7ad99e6a8925b2e2cd4
Victor <victorasantekyei@yahoo.co.uk>
2018-02-26 20:48:45 +0100

    add camera and nav icons

--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import {Icon} from 'native-base';
 
 export class MainScreen extends Component {
+  static navigationOptions = {
+    headerLeft: <Icon name="ios-camera-outline"  style={{paddingLeft: 10}} />,
+    title: "Instagram",
+    headerRight: <Icon name="ios-send-outline" style={{paddingRight: 10}}/>
+  }
   render() {
     return (
       <View>
