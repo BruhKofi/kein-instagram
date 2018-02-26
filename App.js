6102712513e64dbc7e7892c8dab720aacb282b5e
Victor <victorasantekyei@yahoo.co.uk>
2018-02-26 20:40:38 +0100

    add main screen using stack navigator

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { StackNavigator } from "react-navigation";
+import MainScreen from './Components/MainScreen'
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <AppStackNavigator/>
     );
   }
 }
+
+const AppStackNavigator = StackNavigator({
+  Main: {
+    screen: MainScreen
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
---
+++ Components/MainScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class MainScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+export default MainScreen
+
